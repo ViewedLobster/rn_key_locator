11a3e8e545aa45b451fd5d869488429719fb64c9
Simon Sundell <simon.sundell.92@gmail.com>
2015-07-07 22:36:22 +0200

    Modified KeyLocator.py

--- KeyLocator.py
+++ KeyLocator.py
@@ -1,17 +1,49 @@
-import wiringpi2 as wp
+# This optimized piece of code is possible thanks to:
+# pycurl
+# RPi.GPIO
+
+import RPi.GPIO as k
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+pinKey = 18
+pinLed = 27
+
+k.setup(pinLed, k.OUT)
+k.setup(pinKey, k.IN)
+
+i=0
 
 
-wp.piBoardRev()
+def sendAction():
+    
 
-wp.wiringPiSetupGpio()
 
-pinKey = 1
-pinLed = 2
+def main():
+    # TODO better name for variable
+    pinKeyValue = 0
+    done = False
+    while not done:
+        # TODO could prabably improve
+        pinKeyValue = k.input(pinKey)
+        if pinKeyValue == 1:
+            
+        
+        
+        print GPIO.input(button4)
+        time.sleep(1)
+        if GPIO.input(button4)==1:
+            print "fuck yeah"
+            GPIO.output(led, 0)
+        else:
+            GPIO.output(led, 1)
+            
+        if GPIO.input(button0)==1:
+            print "0"
+            break
 
-# pin x som 0: input 1:
-wp.pinMode(pinKey, 0)
-wp.pinMode(pinLed, 1)
 
-# (Pin x, pullDown
-wp.pullUpDnControl(pinKey, 1)
+main()
+
 
