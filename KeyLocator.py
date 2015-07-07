9f467774bfdcbddab295eb17fb4e623ee8164631
Simon Sundell <simon.sundell.92@gmail.com>
2015-07-07 22:48:22 +0200

    Modified KeyLocator.py

--- KeyLocator.py
+++ KeyLocator.py
@@ -6,6 +6,13 @@ import RPi.GPIO as k
 import time
 import pycurl as pc
 
+import pycurl
+from StringIO import StringIO
+
+# Body is a string in some encoding.
+# In Python 2, we can print it without knowing what the encoding is.
+print(body)
+
 k.setmode(k.BCM)
 
 pinKey = 18
@@ -18,8 +25,18 @@ i=0
 
 k.output(pinLed, 1)
 
-time.sleep(10000)
-def sendAction():
+
+def doCurl(keyValue = -1):
+    buffer = StringIO()
+    c = pycurl.Curl()
+    c.setopt(c.URL, 'http://pycurl.sourceforge.net/')
+    c.setopt(c.WRITEDATA, buffer)
+    c.perform()
+    c.close()
+
+    body = buffer.getvalue()
+    print body
+
     pass
     
 
@@ -32,6 +49,9 @@ def main():
         # TODO could prabably improve
         pinKeyValue = k.input(pinKey)
         if pinKeyValue == 1:
+            buffer = StringIO()
+
+
             pass
             
 
