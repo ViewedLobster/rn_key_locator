e807c2d3b2dc5d1748c6fb035881d6df8f634109
Simon Sundell <simon.sundell.92@gmail.com>
2015-07-07 23:05:13 +0200

    Modified KeyLocator.py

--- KeyLocator.py
+++ KeyLocator.py
@@ -8,60 +8,63 @@ import time
 import pycurl
 from StringIO import StringIO
 
+import json
 
-
-k.setmode(k.BCM)
-
-pinKey = 18
-pinLed = 27
-
-k.setup(pinLed, k.OUT)
-k.setup(pinKey, k.IN)
-
-i=0
-
-k.output(pinLed, 1)
-
-
-def doCurl(keyValue = -1):
-    buffer = StringIO()
-    c = pycurl.Curl()
-    address = 'http://rneventteknik.se/stage/io/key.php?key_status='+str(keyValue)
-    print address
-    c.setopt(c.URL, address)
-    c.setopt(c.WRITEFUNCTION, buffer.write)
-    c.perform()
-    c.close()
-
-    body = buffer.getvalue()
+class KeyLocator:
     
-    print body
+    def __init__(self):
+
+    self.jsonDecoder = json.JSONDecoder()
+    k.setmode(k.BCM)
     
-
-
-def main():
-    # TODO better name for variable
-    pinKeyValue = 0
-    done = False
-    while not done:
-        # TODO could prabably improve
-        pinKeyValue = k.input(pinKey)
+    self.pinKey = 18
+    self.pinLed = 27
+    
+    k.setup(self.pinLed, k.OUT)
+    k.setup(self.pinKey, k.IN)
+    
+    k.output(self.pinLed, 1)
+    
+    
+    def doCurl(self, keyValue = -1):
+        buffer = StringIO()
+        c = pycurl.Curl()
+        address = 'http://rneventteknik.se/stage/io/key.php?key_status='+str(keyValue)
+        print address
+        c.setopt(c.URL, address)
+        c.setopt(c.WRITEFUNCTION, buffer.write)
+        c.perform()
+        c.close()
+    
+        body = buffer.getvalue()
         
+        
+        
+    
+    
+    def main():
+        # TODO better name for variable
+        pinKeyValue = 0
+        done = False
+        while not done:
+            # TODO could prabably improve
+            pinKeyValue = k.input(pinKey)
             
-
-        """
-        print GPIO.input(button4)
-        time.sleep(1)
-        if GPIO.input(button4)==1:
-            print "fuck yeah"
-            GPIO.output(led, 0)
-        else:
-            GPIO.output(led, 1)
-            
-        if GPIO.input(button0)==1:
-            print "0"
-            break
-        """
-
-doCurl()
-
+                
+    
+            """
+            print GPIO.input(button4)
+            time.sleep(1)
+            if GPIO.input(button4)==1:
+                print "fuck yeah"
+                GPIO.output(led, 0)
+            else:
+                GPIO.output(led, 1)
+                
+            if GPIO.input(button0)==1:
+                print "0"
+                break
+            """
+    
+    doCurl()
+    
