4e2c17df62ff7ee4d1d8d0209bf5fc1499b6919a
Simon Sundell <simon.sundell.92@gmail.com>
2015-07-07 23:49:26 +0200

    Modified KeyLocator.py

--- KeyLocator.py
+++ KeyLocator.py
@@ -31,7 +31,7 @@ class KeyLocator:
     def doCurl(self, keyValue = -1, messageType = "ping", format = 'json'):
         buffer = StringIO()
         c = pycurl.Curl()
-        address = 'http://rneventteknik.se/stage/io/key.php?key='+str(keyValue)
+        address = 'http://rneventteknik.se/stage/io/key.php?key='+str(keyValue)+"&door="+str(doorValue)
         c.setopt(c.URL, address)
         c.setopt(c.WRITEFUNCTION, buffer.write)
         c.perform()
