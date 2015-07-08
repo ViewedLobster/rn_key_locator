1249bc275ef7dab3f36b976ad7cd19f6584fa32e
Simon Sundell <simon.sundell.92@gmail.com>
2015-07-08 22:23:35 +0200

    Modified KeyLocator.py

--- KeyLocator.py
+++ KeyLocator.py
@@ -19,7 +19,7 @@ class KeyLocator:
         
         self.pinKey = 18
         self.pinLed = 27
-        self.pinAlarm = 2
+        self.pinAlarm = 17
         
         self.LEDState = 0
         
