0092679e27bad4c7103d2621b579bfd75f6374fd
Simon Sundell <simon.sundell.92@gmail.com>
2015-07-08 22:44:08 +0200

    Modified KeyLocator.py

--- KeyLocator.py
+++ KeyLocator.py
@@ -110,7 +110,7 @@ class KeyLocator:
     def doSMS(self, smsString):
         
         pycurl_connect = pycurl.Curl()
-        pycurl_connect.setopt(pycurl.URL, 'https://api.getsupertext.com/v1/conversations/252585/messages')
+        pycurl_connect.setopt(pycurl.URL, 'https://api.getsupertext.com/v1/conversations/44393/messages')
 		# send as Magnus: 0a6f4ffcc6322271ecc6b1ddb90d83720700acc69bd61b7f96e248f7765d
 		# send as Johan: 803647742c19c29a3e7bbbc0eddef9ad49ccfbd22c7d304bd929637036b3
         pycurl_connect.setopt(pycurl.HTTPHEADER, ['Auth-Token: 0a6f4ffcc6322271ecc6b1ddb90d83720700acc69bd61b7f96e248f7765d',
