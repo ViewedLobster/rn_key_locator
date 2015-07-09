fc5c31271c1130a48a81a3295560e4e45a8ed1e6
Simon Sundell <simon.sundell.92@gmail.com>
2015-07-09 22:36:04 +0200

    Modified test.py

--- test.py
+++ test.py
@@ -1,8 +1,12 @@
+# -*- coding: utf-8 -*-
 import RPi.GPIO as k
 import time
 
 k.setmode(k.BCM)
 pins = [2,3,4,5,6,12,13,16,17,18,19,20,21,22,23,24,25,26]
+foo = "åäö"
+
+print foo
 
 
 for pin in pins:
