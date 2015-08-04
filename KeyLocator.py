e7057ac51cda8d0ccf2d03ed770fbbece5175492
Simon Sundell <simon.sundell.92@gmail.com>
2015-08-04 19:46:47 +0200

    Modified KeyLocator.py

--- KeyLocator.py
+++ KeyLocator.py
@@ -156,8 +156,8 @@ class KeyLocator:
         # test-api: 252585
 		# send as Magnus: 0a6f4ffcc6322271ecc6b1ddb90d83720700acc69bd61b7f96e248f7765d
 		# send as Johan: 803647742c19c29a3e7bbbc0eddef9ad49ccfbd22c7d304bd929637036b3
-        #send as: Rn Key Defence System 803647742c19c29a3e7bbbc0eddef9ad49ccfbd22c7d304bd929637036b3 0763035548
-        pycurl_connect.setopt(pycurl.HTTPHEADER, ['Auth-Token: 803647742c19c29a3e7bbbc0eddef9ad49ccfbd22c7d304bd929637036b3',
+        #send as: Rn Key Defence System 6cd627bf1b1c7aa9d4a980b73dac83984d7b70b5eaaaf3212c1d968cbec4 0763035548
+        pycurl_connect.setopt(pycurl.HTTPHEADER, ['Auth-Token: 6cd627bf1b1c7aa9d4a980b73dac83984d7b70b5eaaaf3212c1d968cbec4',
                                                   'application/json, text/javascript, */*; q=0.01'])
         pycurl_connect.setopt(pycurl.POSTFIELDS, "message="+str(smsString)+"&send_to_self=1")
         pycurl_connect.perform()
