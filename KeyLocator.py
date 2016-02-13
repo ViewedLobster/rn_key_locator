2b829e4681e088aca20ef84c727a958dfe8e3afd
Magnus Arvidsson <magnus.arvidsson@tvesovla.se>
2016-02-14 00:13:53 +0100

    hahahahha

--- KeyLocator.py
+++ KeyLocator.py
@@ -13,6 +13,7 @@ from StringIO import StringIO
 
 import json
 import logging
+import random
 
 class KeyLocator:
     
@@ -34,7 +35,15 @@ class KeyLocator:
         k.setup(self.ledPin, k.OUT)
         k.setup(self.keyPin, k.IN)
         k.setup(self.doorPin, k.IN)
+
+        # An assortment of goods to be owed
+        self.goodsList = ["bulle", "Guldkällan", "lap dance", "juice", "tårta", "crystal meth", "saft", "lurre till Jurre", "virre", "chirre", "rent mjöl, gärna i påse", "TeX:ad kanallista"]
         
+        self.amountList = ["skälig mängd", "fyra dagsböter", "fem fotbollplaner", "fyrtio tusen miljarder", "33 cl", "1 kg"]
+        
+        self.explanation = ["Straffet bestäms utifrån den misstänktes beräknade årsinkomst vid tidpunkten när straffet skall bestämmas. Med årsinkomst avses inkomst före skatt med avdrag för kostnader för inkomstens förvärvande. Som inkomst räknas också mer kontinuerligt utgående ersättning eller bidrag till den misstänkte. Det gäller till exempel arbetslöshetsersättning, ekonomiskt bistånd, studiebidrag, bostadsbidrag och underhållsbidrag.", 
+                "Den misstänktes egna uppgifter om sina ekonomiska förhållanden bör i allmänhet godtas. En kontroll bör dock göras om det finns anledning anta att uppgifterna är felaktiga eller ofullständiga i en omfattning som kan antas påverka straffbeloppet.",
+                "Straffets villkor varierar mellan olika nämnder. Anstalten RN ger möjlighet till arbete, utbildning, behandlingsprogram och annan verksamhet. Straff infördes hos RN under 1800-talet och anses bland annat minska risken för fängelsevåldtäkter"]
         
         self.keyState = None
         self.doorState = None
@@ -53,6 +62,16 @@ class KeyLocator:
         # Hahaha, much secure
         self.checksum = "67fgd83kdn3249f34nnjf27d2lmkcds"
     
+    def generateMessage():
+        message = "Kom tillbaka med nyckeln, om den ej är tillbaka inom 10 minuter blir påföljden ett vite i form av "
+        i = random.randrange(len(self.amountList));
+        message = message + self.amountList[i]
+
+        i = random.randrange(len(self.goodsList))
+        message = message + " " + self.goodsList[i]
+
+        i = random.randrange(len(self.explanation))
+        message = message + " " + self.explanation[i]
     
     def doCurl(self, keyValue = -1, doorValue = -1, emergencyState = -1, messageType = "ping", format = 'json'):
         buffer = StringIO()
@@ -74,7 +93,9 @@ class KeyLocator:
     
     def main(self):
         
+        # keyState is true if key is in place
         self.keyState = k.input(self.keyPin)
+        # DoorState is true if door is locked
         self.doorState = k.input(self.doorPin)
         self.lastKeyState = self.keyState
         self.lastDoorState = self.doorState
@@ -95,8 +116,10 @@ class KeyLocator:
                 if self.emergency and self.keyState and not self.doorState:
                     
                     self.emergency = False
-                    
-                    
+
+                    # Key is back and all is well
+
+                    """
                     f = open(self.emergencyFile, "r")
                     string = f.readline()
                     f.close()
@@ -109,6 +132,7 @@ class KeyLocator:
                     f = open(self.emergencyFile, "w")
                     f.write("None")
                     f.close()
+                    """
                     
                     # send back to normal message
                     if string != "None":
@@ -119,16 +143,11 @@ class KeyLocator:
                 elif not self.keyState and self.doorState:
                     self.emergency = True
                     
+                    # If door locked and key gone fishing
                     
-                    
-                    f = open(self.emergencyFile, "r")
-                    string = f.readline()
-                    f.close()
-                    if string == "None":
-                        f = open(self.emergencyFile, "w")
-                        f.write(str(time.time()))
-                        self.doSMS(self.smsBad)
-                        f.close()
+                    smsString = self.generateMessage()
+                    self.doSMS(smsString)
+
                     
                 response = self.doCurl(self.keyState, self.doorState, self.emergency, "changed")
                 
@@ -160,13 +179,13 @@ class KeyLocator:
     def doSMS(self, smsString):
         
         pycurl_connect = pycurl.Curl()
-        pycurl_connect.setopt(pycurl.URL, 'https://api.getsupertext.com/v1/conversations/44393/messages')
+        pycurl_connect.setopt(pycurl.URL, 'https://api.getsupertext.com/v1/conversations/252585/messages')
         # rn-eventteknik: 44393
         # test-api: 252585
 		# send as Magnus: 0a6f4ffcc6322271ecc6b1ddb90d83720700acc69bd61b7f96e248f7765d
 		# send as Johan: 803647742c19c29a3e7bbbc0eddef9ad49ccfbd22c7d304bd929637036b3
         #send as: Rn Key Defence System 6cd627bf1b1c7aa9d4a980b73dac83984d7b70b5eaaaf3212c1d968cbec4 0763035548
-        pycurl_connect.setopt(pycurl.HTTPHEADER, ['Auth-Token: 6cd627bf1b1c7aa9d4a980b73dac83984d7b70b5eaaaf3212c1d968cbec4',
+        pycurl_connect.setopt(pycurl.HTTPHEADER, ['Auth-Token: 803647742c19c29a3e7bbbc0eddef9ad49ccfbd22c7d304bd929637036b3',
                                                   'application/json, text/javascript, */*; q=0.01'])
         pycurl_connect.setopt(pycurl.POSTFIELDS, "message="+str(smsString)+"&send_to_self=1")
         try:
